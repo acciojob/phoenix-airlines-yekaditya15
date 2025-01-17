fc4aee59d884f10e02bbb19d6de9b1b42bbd9ba6
yekaditya11 <yekaditya11@gmail.com>
2025-01-17 20:59:28 +0530

    update1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import {render} from 'react-dom';
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import createSagaMiddleware from "redux-saga";
 
+import Home from "./modules/home/home";
 
+import rootReducer from "./rootReducer";
+import rootSaga from "./sagas";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const sagaMiddleWare = createSagaMiddleware();
+
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleWare));
+sagaMiddleWare.run(rootSaga);
+
+const rootElement = document.getElementById("root");
+
+render(
+  <Provider store={store}>
+    <Home />
+  </Provider>
+  ,
+  rootElement
+);
